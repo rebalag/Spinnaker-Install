d400076f146c143a3ba69b116457e3e891fad72c
Sagnik Ghosh <36922114+sagnik-opsmx@users.noreply.github.com>
2018-12-26 19:07:15 +0530

    Update spinnaker-lite-install.sh

--- spinnaker-oc-install/spinnaker-lite-install.sh
+++ spinnaker-oc-install/spinnaker-lite-install.sh
@@ -82,7 +82,8 @@ sed -i "s/opsmx_123456/$secret_access_key/" halconfigmap_template.yml
 #sed -i "s/spin-deck.abc.com/$deckurl/" halconfigmap_template.yml
 #Applying Halconfig template and Halyard Deployment Pod 
 printf "\n  [****] Applying The Halyard local BOM [****] "
-curl https://raw.githubusercontent.com/OpsMx/Spinnaker-Install/master/spinnaker-oc-install/1.11.2-bom
+curl https://raw.githubusercontent.com/OpsMx/Spinnaker-Install/master/spinnaker-oc-install/1.11.2-bom -o 1.11.2.yml
+kubectl create configmap bomconfig --from-file=1.11.2.yml -n spinnaker
 
 printf "\n  [****] Applying The Halyard ConfigMap, Secrets and the Halyard Deployment Pod [****] "
 printf '\n'
