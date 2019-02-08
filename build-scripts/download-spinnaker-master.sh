a6fa116dfdc421d3e374757bb75353cbd9b7a203
j-sandy <30489233+j-sandy@users.noreply.github.com>
2019-02-08 22:16:25 +0530

    Create script for cloning spinnaker repo

---
+++ build-scripts/download-spinnaker-master.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git clone https://github.com/spinnaker/spinnaker.git 
+git clone https://github.com/spinnaker/clouddriver.git 
+git clone https://github.com/spinnaker/gate.git 
+git clone https://github.com/spinnaker/deck.git 
+git clone https://github.com/spinnaker/fiat.git 
+git clone https://github.com/spinnaker/orca.git 
+git clone https://github.com/spinnaker/rosco.git 
+git clone https://github.com/spinnaker/igor.git 
+git clone https://github.com/spinnaker/kayenta.git 
+git clone https://github.com/spinnaker/halyard.git 
+git clone https://github.com/spinnaker/front50.git 
+git clone https://github.com/spinnaker/echo.git 
+git clone https://github.com/spinnaker/spin.git 
+git clone https://github.com/spinnaker/spinnaker-dependencies.git 
+git clone https://github.com/spinnaker/spinnaker-monitoring.git 
